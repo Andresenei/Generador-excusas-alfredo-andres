6483eda518e874dbaf8db4a31e706df7c195b260
Andresenei <andresenei@gmail.com>
2025-04-20 21:25:52 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ["Mi perro", "Mi novia", "El profesor", "Mi vecina"];
+let action = ["Se comió", "orinó", "estrelló", "perdió"];
+let what = ["mi tarea", "mi teléfono", "mi dinero", "mi coche"];
+let when = ["justo antes de la clase", "mientras dormía", "mientras me ejercitaba", "durante el almuerzo"];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandom(array) {
+  let randomIndex = getRandomNumber(0, array.length - 1);
+  return array[randomIndex];
+}
+
+window.onload = function () {
+  const excuseElement = document.getElementById("excuse");
+  const excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}.`;
+  excuseElement.textContent = excuse;
 };
